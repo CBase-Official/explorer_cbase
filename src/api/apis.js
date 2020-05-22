547bdb17a255c86f9cee74af7793e3707a60b0c9
welling <welling1234@gmail.com>
2020-05-22 10:16:39 +0800

    U

--- src/api/apis.js
+++ src/api/apis.js
@@ -3,11 +3,11 @@ var axios = require("axios");
 // var near_url = "http://112.74.106.123:3030";
 
 // var data_url = "http://47.57.7.136:3000/"
-var near_url = "http://18.163.235.28:3030"
+// var near_url = "http://18.163.235.28:3030"
 
-// var data_url = "http://192.168.80.54:3000/"
-var data_url = "http://192.168.1.104:3000/"
-// var near_url = "http://192.168.80.54:3030"
+var data_url = "http://192.168.80.54:3000/"
+// var data_url = "http://192.168.1.108:3000/"
+var near_url = "http://192.168.80.54:3030"
 // var data_url = "http://47.90.101.201:3000/";
 // var near_url = "http://47.90.101.201:3030"
 
